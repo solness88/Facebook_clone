5ee9439d0157959fd161b875e6da66841d95bccc
hirokazu kobayashi <solness.stf@gmail.com>
2021-01-02 17:14:56 +0900

    友達つながり機能を追加中

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 class CommentsController < ApplicationController
+
+  before_action :set_blog, only: [:create, :edit, :update]
+
   def create
-    @blog = Blog.find(params[:blog_id])
     @comment = @blog.comments.build(comment_params)
     respond_to do |format|
       if @comment.save
@@ -10,8 +12,45 @@ class CommentsController < ApplicationController
       end
     end
   end
+
+  def edit
+    @comment = @blog.comments.find(params[:id])
+    respond_to do |format|
+      flash.now[:notice] = 'コメントの編集中'
+      format.js { render :edit }
+    end
+  end
+
+  def update
+    @comment = @blog.comments.find(params[:id])
+      respond_to do |format|
+        if @comment.update(comment_params)
+          flash.now[:notice] = 'コメントが編集されました'
+          format.js { render :index }
+        else
+          flash.now[:notice] = 'コメントの編集に失敗しました'
+          format.js { render :edit_error }
+        end
+      end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    respond_to do |format|
+      flash.now[:notice] = 'コメントが削除されました'
+      format.js { render :index }
+    end
+  end
+
   private
+
   def comment_params
     params.require(:comment).permit(:blog_id, :content)
   end
+
+  def set_blog
+    @blog = Blog.find(params[:blog_id])
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
+  has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
